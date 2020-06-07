4b7616bb950f0b464dbb44aeec825cb5739cc169
annapostn <42993054+annapostn@users.noreply.github.com>
2020-06-07 13:22:18 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import url_for, render_template, request, redirect
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  if request.method == 'POST':
+    file = request..files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+  return render_template(index.html)
+  
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+if __name__ == '___main___':
+  app.run()
