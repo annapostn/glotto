1257d9c87beeb92559fddae34d8fbead870b218c
annapostn <42993054+annapostn@users.noreply.github.com>
2020-06-07 13:55:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import url_for, render_template, request, redirect
 
+UPLOAD_FOLDER = '/'
+ALLOWED_EXTENSIONS = set(['txt'])
+
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -8,12 +11,12 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/', methods=['GET', 'POST'])
 def index():
   if request.method == 'POST':
-    file = request..files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
   return render_template(index.html)
   
 from flask import send_from_directory
